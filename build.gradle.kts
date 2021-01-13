ca1107dbdfd58dd7b8a7e70fc80ab34c59cdbcf2
Bo Zhang <zhangbodut@gmail.com>
2021-01-13 10:49:33 +0800

    Upgrade wrapper to 6.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,17 +18,17 @@ configurations.all {
 }
 
 dependencies {
-    compile("io.airlift:airline:0.7")
-    compile("org.eclipse.jgit:org.eclipse.jgit:4.9.1.201712030800-r")
-    compile("org.jetbrains.kotlin:kotlin-stdlib")
-    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.airlift:airline:0.7")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:4.9.1.201712030800-r")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    runtime("org.slf4j:slf4j-simple:1.7.25")
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.25")
 
-    testCompile(gradleTestKit())
-    testCompile("org.codehaus.groovy:groovy:2.4.10")
-    testCompile("org.spockframework:spock-core:1.0-groovy-2.4")
-    testRuntime("cglib:cglib-nodep:2.2.2")
+    testImplementation(gradleTestKit())
+    testImplementation("org.codehaus.groovy:groovy:2.4.10")
+    testImplementation("org.spockframework:spock-core:1.0-groovy-2.4")
+    testRuntimeOnly("cglib:cglib-nodep:2.2.2")
 }
 
 java {
