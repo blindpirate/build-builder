c5b94319e8ec4a8a421c71393ca62b5bb301791f
Paul Merlin <paul@gradle.com>
2022-09-30 12:35:58 +0200

    Publish build scans on CI env

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+plugins {
+    id("com.gradle.enterprise") version ("3.11.1")
+}
+
 rootProject.name = "build-builder"
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
